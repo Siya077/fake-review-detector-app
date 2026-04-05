9954fcbc7ddc44e336280c65767f67c83086de00
Siya077 <149461346+Siya077@users.noreply.github.com>
2026-04-05 23:59:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pickle
 
+# ✅ DEBUG START
+st.write("App started")
+
+st.write("Loading model...")
+model = pickle.load(open("model.pkl", "rb"))
+
+st.write("Loading vectorizer...")
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
+st.write("Loaded successfully ✅")
+# ✅ DEBUG END
+
+# Your app UI
+st.title("Fake Review Detection System")
